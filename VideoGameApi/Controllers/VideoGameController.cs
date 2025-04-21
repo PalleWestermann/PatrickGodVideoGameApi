3e1656025f6a73ea202ff0b3969af15fa3b04043
Palle Westermann <pwe@tec.dk>
2025-04-21 11:18:45 +0200

    BeforeHavingDataBase

--- VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -60,5 +60,31 @@ namespace VideoGameApi.Controllers
             videoGames.Add(newGame);
             return CreatedAtAction(nameof(GetVideoGameById), new {id = newGame.Id}, newGame);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateVideoGame(int id, VideoGame updatedGame)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+
+            game.Title = updatedGame.Title;
+            game.Platform = updatedGame.Platform;
+            game.Developer = updatedGame.Developer;
+            game.Publisher = updatedGame.Publisher;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteVideoGame(int id) 
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+
+            videoGames.Remove(game);
+            return NoContent();
+        }
     }
 }
