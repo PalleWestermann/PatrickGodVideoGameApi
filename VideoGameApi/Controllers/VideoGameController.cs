d1d23b5a10c8eb03871b44ffe3bf8be75a1b0e8f
Palle Westermann <pwe@tec.dk>
2025-04-21 10:49:41 +0200

    Add project files.

---
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new List<VideoGame>()
+        { 
+            new VideoGame
+            {
+                Id = 1,
+                Title = "Spider-Man 2",
+                Platform = "PS5",
+                Developer = "Insomniac Games",
+                Publisher = "Sony Interactive Entertainment"
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "The Legend of Zelda; Breath of the Wild",
+                Platform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Cyperpunk 2077",
+                Platform = "PC",
+                Developer = "CD Project Red",
+                Publisher = "CD Project"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<VideoGame> GetVideoGameById(int id) 
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+                return BadRequest();
+
+            newGame.Id = videoGames.Max(g => g.Id) + 1; // No database to give us id. So, we do it.
+            videoGames.Add(newGame);
+            return CreatedAtAction(nameof(GetVideoGameById), new {id = newGame.Id}, newGame);
+        }
+    }
+}
